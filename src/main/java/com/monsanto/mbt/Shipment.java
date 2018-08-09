f4279d2222a59dd0c491b070f284f01ba399f338
vcumapathi <umapathi.v7@gmail.com>
2018-08-09 16:14:53 +0530

    coding test latest source code

--- src/main/java/com/monsanto/mbt/Shipment.java
+++ src/main/java/com/monsanto/mbt/Shipment.java
@@ -1,9 +1,84 @@
 package com.monsanto.mbt;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
 public class Shipment {
+	
+	 public static List<Widget> getSampleWidgets() {
+	        List<Widget> widgets = new ArrayList<Widget>();
 
+	        widgets.add(new Widget(1, "Red", stringToDateConverter("01-01-2006")));
+	        widgets.add(new Widget(2, "Blue",  stringToDateConverter("02-03-2007")));
+	        widgets.add(new Widget(3, "Red",  stringToDateConverter("03-09-2006")));
+	        widgets.add(new Widget(4, "Green", stringToDateConverter( "04-01-2009")));
+	        widgets.add(new Widget(5, "Green",  stringToDateConverter("05-07-2011")));
+	        widgets.add(new Widget(6, "Blue", stringToDateConverter( "06-02-2016")));
+	        widgets.add(new Widget(7, "Green",  stringToDateConverter("07-01-2008")));
+	        widgets.add(new Widget(8, "Blue",  stringToDateConverter("08-02-2009")));
+	        widgets.add(new Widget(9, "Red",  stringToDateConverter("09-01-2005")));
+	        widgets.add(new Widget(10, "Blue",  stringToDateConverter("01-03-2013")));
+	        widgets.add(new Widget(22, "Blue",  stringToDateConverter("02-13-2007")));
+	        widgets.add(new Widget(23, "Red",  stringToDateConverter("03-14-2006")));
+	        widgets.add(new Widget(25, "Green",  stringToDateConverter("05-16-2011")));
+	        widgets.add(new Widget(24, "Red",  stringToDateConverter("04-15-2009")));
 
+	        return widgets;
+	    }
 
+	    public static Date stringToDateConverter(String strDate){
+	        Date parsedDate = null;
+	        SimpleDateFormat formatter = new SimpleDateFormat("MM-dd-yyyy");
+	        try {
+	            parsedDate = formatter.parse(strDate);
+	        } catch (ParseException e) {
+	            e.printStackTrace();
+	        }
+	        return parsedDate;
+	    }
 
+	public static void main(String[] args) {
 
+		List<Widget> empArr1 = Shipment.getSampleWidgets();
+		Widget[] empArr  =empArr1.toArray(new Widget[empArr1.size()]);
+		 Arrays.sort(empArr);
+        
+        Arrays.sort(empArr, Widget.colorComparator);
+        System.out.println("Widget list sorted by color");
+        
+
+        System.out.println("-----------------------------------------------------------------------------");
+        System.out.printf("%10s %30s %50s", "Serial No", "Color", "Production Date");
+        System.out.println();
+        System.out.println("-----------------------------------------------------------------------------");
+        for(Widget student: empArr){
+            System.out.format("%10s %30s %50s",
+                    student.getSerialNumber(), student.getColor(), student.getProductionDate());
+            System.out.println();
+        }
+        System.out.println("-----------------------------------------------------------------------------");
+
+        
+        Arrays.sort(empArr, Widget.productionDateComparator);
+        System.out.println("Widget list sorted by  Production Date");
+        
+        System.out.println();
+        System.out.println("-----------------------------------------------------------------------------");
+        System.out.printf("%10s %30s %50s","Serial No", "Color", "Production Date");
+        System.out.println();
+        System.out.println("-----------------------------------------------------------------------------");
+        for(Widget student: empArr){
+            System.out.format("%10s %30s %50s",
+                    student.getSerialNumber(), student.getColor(), student.getProductionDate());
+            System.out.println();
+        }
+        System.out.println("-----------------------------------------------------------------------------");
+
+        
+       }
+	
 }
--- src/main/java/com/monsanto/mbt/Widget.java
+++ src/main/java/com/monsanto/mbt/Widget.java
@@ -1,9 +1,10 @@
 package com.monsanto.mbt;
 
+import java.util.Comparator;
 import java.util.Date;
 
 
-public class Widget {
+public class Widget implements Comparable<Widget>{
     private int serialNumber;
     private String color;
     private Date productionDate;
@@ -37,4 +38,67 @@ public class Widget {
     public void setProductionDate(Date productionDate) {
         this.productionDate = productionDate;
     }
+
+	public int compareTo(Widget o) {
+		
+		return o.serialNumber;
+	}
+
+	@Override
+	public String toString() {
+		return "Widget [serialNumber=" + serialNumber + ", color=" + color + ", productionDate=" + productionDate + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((color == null) ? 0 : color.hashCode());
+		result = prime * result + ((productionDate == null) ? 0 : productionDate.hashCode());
+		result = prime * result + serialNumber;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Widget other = (Widget) obj;
+		if (color == null) {
+			if (other.color != null)
+				return false;
+		} else if (!color.equals(other.color))
+			return false;
+		if (productionDate == null) {
+			if (other.productionDate != null)
+				return false;
+		} else if (!productionDate.equals(other.productionDate))
+			return false;
+		if (serialNumber != other.serialNumber)
+			return false;
+		return true;
+	}
+	
+	   public static final Comparator<Widget> colorComparator = new Comparator<Widget>(){
+
+	        public int compare(Widget o1, Widget o2) {
+	            return o1.color.compareTo(o2.color);
+	        }
+	      
+	    };
+	  
+	    public static final Comparator<Widget> productionDateComparator = new Comparator<Widget>(){
+
+	        public int compare(Widget o1, Widget o2) {
+	            return o1.productionDate.compareTo(o2.productionDate);
+	        }
+	      
+	    };
+
+
+
 }
